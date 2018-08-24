c964c70bc7710472cf8aef7a6a6a423ad05f4a7f
rquast <r.quast@uq.edu.au>
2018-08-24 15:57:19 +1000

    saves favourite field data

--- src/AppBundle/Controller/CharactersController.php
+++ src/AppBundle/Controller/CharactersController.php
@@ -5,19 +5,38 @@ namespace AppBundle\Controller;
 use AppBundle\Service\CharactersService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
 class CharactersController extends Controller
 {
 
     /**
+     * @param CharactersService $charactersService
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     *
      * @Route("/api/characters", name="characters")
      */
     public function indexAction(CharactersService $charactersService)
     {
-
         $characters = $charactersService->findAll();
-
         return $this->json($characters);
+    }
+
+    /**
+     * @param $id string Character ID
+     * @param $request Request request object
+     * @param CharactersService $charactersService
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     *
+     * @Route("/api/characters/{id}", name="update-characters")
+     * @Method("PATCH")
+     */
+    public function updateAction($id, Request $request, CharactersService $charactersService)
+    {
+        $data = json_decode($request->getContent(), true);
+        $character = $charactersService->update($id, $data);
+        return $this->json($character);
     }
 
 }
--- src/AppBundle/Service/CharactersService.php
+++ src/AppBundle/Service/CharactersService.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Entity\Characters;
+use Doctrine\ORM\EntityManager;
+
 
 class CharactersService extends BaseService {
 
@@ -10,6 +13,35 @@ class CharactersService extends BaseService {
         $marvelApiService = new MarvelApiService();
         $marvelApiService->setContainer($this->container);
         return $marvelApiService->query();
+    }
+
+    public function update($id, $data) {
+
+        /**
+         * @var $em EntityManager
+         */
+        $em = $this->container->get('doctrine')->getManager();
+        $characterRepository = $em->getRepository(Characters::class);
+        $character = $characterRepository->find($id);
+        $character->setFavourite($data['data']['attributes']['favourite']);
+        $em->persist($character);
+
+        $em->flush();
+
+        return [
+            "data" => [
+                "type" => "character",
+                "id" => $id,
+                "attributes" => [
+                    "name" => $character->getName(),
+                    "description" => $character->getDescription(),
+                    "thumbnail" => $character->getThumbnail(),
+                    "resource-uri" => $character->getResourceUri(),
+                    "favourite" => $character->isFavourite(),
+                    "modified" => $character->getModified()
+                ]
+            ]
+        ];
     }
 
 }
