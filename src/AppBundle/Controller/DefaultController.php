37a3a3a9649fbf60671661b0fc3bbf41e0afffc8
rquast <r.quast@uq.edu.au>
2018-08-27 10:30:00 +1000

    creates jwt tokens now

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Security\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $jwtManager = $this->container->get('lexik_jwt_authentication.jwt_manager');
+        $user = new User('uqrquast', ['ROLE_USER'], 'r.quast@uq.edu.au');
+        return $this->json(['token' => $jwtManager->create($user)]);
     }
+
 }
