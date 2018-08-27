1ad86bc8026b4f9a5a24635854dff8809319e896
rquast <r.quast@uq.edu.au>
2018-08-27 13:45:18 +1000

    allow options in secure route

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -30,33 +30,12 @@ class LoginController extends Controller
     public function loginAction(Request $request)
     {
 
-        /*
-
-        Return Example:
-
-            {
-                "authenticator": "authenticator:jwt",
-                "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJhYmMxMjMiLCJpYXQiOjE1MzUzMzA5MjUsImV4cCI6MTUzNTMzNDczM30.6ARxNzS198wvGz8RR8T80Zk00SEAryGhiHarx6S2fCs",
-                "exp": 1535334733,
-                "tokenData": {
-                    "username": "admin",
-                    "password": "abc123",
-                    "iat": 1535330925,
-                    "exp": 1535334733
-                }
-            }
-
-         */
-
+        // TODO: "validate" the username / password
+        // $data = json_decode($request->getContent(), true);
 
         $jwtManager = $this->container->get('lexik_jwt_authentication.jwt_manager');
         $user = new User('uqrquast', ['ROLE_USER'], 'r.quast@uq.edu.au');
         return $this->json(['token' => $jwtManager->create($user)]);
-
-
-
-        // $data = json_decode($request->getContent(), true);
-        // return $this->json($data);
     }
 
 }
